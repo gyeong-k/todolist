d93eb4d37ba15049f06a2b69e658099c90820a3e
gayeongkim <gy.kim.g0@gmail.com>
2024-10-30 17:15:36 +0900

    변수 사용하기, 삼항연산자

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="container">
-      <h1 className="test">hello react</h1>
-      <p>반갑습니다.</p>
-    </div>
-  );
+  // let name = "리액트";
+  // return (
+  //   <div className="container">
+  //     <h1 className="test">
+  //       hello
+  //       {name === "리액트" ? <h1>YES</h1> : null}
+  //     </h1>
+  //     <p>반갑습니다.</p>
+  //   </div>
+  // );
+  let port = undefined;
+  return <div>{port || "포트를 설정하지 않았습니다"}</div>;
 }
 
 export default App;
