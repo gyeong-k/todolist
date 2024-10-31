4d05387eec339db7a13f786199a337631b3a420f
gayeongkim <gy.kim.g0@gmail.com>
2024-10-31 19:02:36 +0900

    현재 시각 넣기

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
 
-const Timer: React.FC = () => {
-  const [seconds, setSeconds] = useState<number>(0);
-  return (
-    <div>
-      <h2>타이머 : {seconds} 초</h2>
-      <button
-        onClick={function () {
-          setInterval(() => {
-            setSeconds((prev) => prev + 1); //이전 초에 1씩 더하기
-          }, 1000);
-        }}
-      >
-        시작
-      </button>
-    </div>
-  );
+// const Timer: React.FC = () => {
+//   const [seconds, setSeconds] = useState<number>(0);
+//   return (
+//     <div>
+//       <h2>타이머 : {seconds} 초</h2>
+//       <button
+//         onClick={function () {
+//           setInterval(() => {
+//             setSeconds((prev) => prev + 1); //이전 초에 1씩 더하기
+//           }, 1000);
+//         }}
+//       >
+//         시작
+//       </button>
+//     </div>
+//   );
+// };
+
+const Clock: React.FC = () => {
+  const [time, setTime] = useState(new Date());
+  setInterval(() => {
+    setTime(new Date());
+  }, 1000);
+  return <div>현재시간 : {time.toLocaleTimeString()}</div>;
 };
 
-export default Timer;
+export default Clock;
