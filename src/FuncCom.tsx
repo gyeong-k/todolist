3825f00889256b8901960398db287181b403a82c
gayeongkim <gy.kim.g0@gmail.com>
2024-10-31 16:43:09 +0900

    함수형 컴포넌트

---
+++ src/FuncCom.tsx
@@ -0,0 +1,9 @@
+//그냥 return만해도 알아서 랜더링함
+// function FuncCom() {
+//   return <div>함수형 컴포넌트</div>;
+// }
+//화살표 함수
+const FuncCom = () => {
+  return <div>함수형 컴포넌트</div>;
+};
+export default FuncCom;
