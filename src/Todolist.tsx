49b80062e7ec18eb5e15eb1dfc70d7f00bce08be
gayeongkim <gy.kim.g0@gmail.com>
2024-10-31 17:52:21 +0900

    type 정의 후 사용

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 
+//type : 가독성, 유지보수
+//인터페이스 안쓴이유 -> 기존의 타입을 사용해 새로운 타입
+//인터페이스 -> 새로운 객체 구조
+type Todo = {
+  id: number;
+  text: string;
+  isChecked: boolean;
+};
+
 //React.FC : 함수컴포넌트 -> 프롭스의 타입 명시 역할
 //state : 리액트에선 변수대신 데이터 저장 -> 데이터의 상태 / 데이터를 동적 감시해 데이터가 변하면 반영
 //state를 변경하기 위해선 별도의 변경함수 필요 : setTodos => 멤버함수같은 개념
 const TodoList: React.FC = () => {
   const title: string = "오늘 할 일";
   //구조문해할당
-  const [todos, setTodos] = useState<string[]>([
-    "공부하기",
-    "잠자기",
-    "미팅하기",
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 1, text: "공부하기", isChecked: false },
+    { id: 2, text: "잠자기", isChecked: false },
+    { id: 3, text: "미팅하기", isChecked: false },
   ]); //state의 데이터, 변경함수 순서
   return (
     <div>
@@ -18,9 +27,9 @@ const TodoList: React.FC = () => {
       <div className="container">
         <div className="board">
           <ul>
-            <li>{todos[0]}</li>
-            <li>{todos[1]}</li>
-            <li>{todos[2]}</li>
+            <li>{todos[0].text}</li>
+            <li>{todos[1].text}</li>
+            <li>{todos[2].text}</li>
           </ul>
         </div>
       </div>
