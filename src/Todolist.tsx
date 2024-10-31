35b0046fd59c28b8a7538b19a9e94e48478a7d96
gayeongkim <gy.kim.g0@gmail.com>
2024-10-31 17:39:18 +0900

    state 로 변수 사용하기

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -2,19 +2,27 @@ import React, { useState } from "react";
 
 //React.FC : 함수컴포넌트 -> 프롭스의 타입 명시 역할
 //state : 리액트에선 변수대신 데이터 저장 -> 데이터의 상태 / 데이터를 동적 감시해 데이터가 변하면 반영
+//state를 변경하기 위해선 별도의 변경함수 필요 : setTodos => 멤버함수같은 개념
 const TodoList: React.FC = () => {
   const title: string = "오늘 할 일";
-  const [todos] = useState(["공부하기", "잠자기", "미팅하기"]);
+  //구조문해할당
+  const [todos, setTodos] = useState<string[]>([
+    "공부하기",
+    "잠자기",
+    "미팅하기",
+  ]); //state의 데이터, 변경함수 순서
   return (
     <div>
       <h1>{title}</h1>
       <p></p>
       <div className="container">
-        <ul>
-          <li>{todos[0]}</li>
-          <li>{todos[1]}</li>
-          <li>{todos[2]}</li>
-        </ul>
+        <div className="board">
+          <ul>
+            <li>{todos[0]}</li>
+            <li>{todos[1]}</li>
+            <li>{todos[2]}</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
